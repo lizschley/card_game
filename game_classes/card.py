a8365c10bd7411515f6bc54bc5fd19574a17c36e
lizschley <lizschley@gmail.com>
2020-10-21 18:31:55 -0400

    Add documentation

--- game_classes/card.py
+++ game_classes/card.py
@@ -10,20 +10,48 @@ COLOR_VALUES = {'red': 3,
 class Card:
     ''' One card used in card game'''
     def __init__(self, color, card_value, color_sort=0):
+        '''
+        __init__ initializes the cards
+
+        :param color: color name for the card
+        :type color: string
+        :param card_value: number that is past in; controlled by the game
+        :type card_value: int
+        :param color_sort: this can be changed, based on the input array when sorting, defaults to 0
+        :type color_sort: int, optional
+        '''
         self.color_name = color
         self.color_value = COLOR_VALUES.get(color, 8)
         self.card_value = card_value
         self.color_sort = color_sort
 
     def __eq__(self, other_card):
+        '''
+        __eq__ used for sorting
+
+        :param other_card: card that is being compared against
+        :type other_card: Card
+        '''
         self.color_sort == other_card.color_sort and self.card_value == other_card.card_value
 
     def __lt__(self, other_card):
+        '''
+        __lt__ used for sorting
+
+        :param other_card: card that is being compared against
+        :type other_card: Card
+        '''
         if self.color_sort < other_card.color_sort:
             return True
         return self.card_value < other_card.card_value
 
     def __str__(self):
+        '''
+        __str__ used so that the card can be printed out as part of taking a turn
+
+        :return: string describing the Card
+        :rtype: string
+        '''
         return (f'Card --> color_name: {self.color_name}, '
                 f'color value: {self.color_value}, '
                 f'card value: {self.card_value}, '
@@ -31,7 +59,22 @@ class Card:
                 f'total_value: {self.color_value * self.card_value}')
 
     def total_value(self):
+        '''
+        total_value returns the value of the Card
+
+        :return: color_value * card_value
+        :rtype: int
+        '''
         return self.color_value * self.card_value
 
     def assign_new_color_sort(self, num):
+        '''
+        assign_new_color_sort if sorting, you pass in an array of color names and the color_sort
+        is the index of the array
+
+        But in the function can be any number
+
+        :param num: list index
+        :type num: int
+        '''
         self.color_sort = num
--- game_classes/card_game.py
+++ game_classes/card_game.py
@@ -33,7 +33,8 @@ class CardGame:
 
     def play_game(self):
         '''
-        play_game driver to play the game
+        play_game driver playing the game.  First make, then shuffle the deck
+        Then you play the game in a loop
         '''
         self.make_card_deck()
         self.shuffle_deck()
@@ -79,14 +80,24 @@ class CardGame:
 
     def end_game(self):
         '''
-        end_game when the game ends, a winner is determined
-
-        [extended_summary]
+        end_game ends the game and prints the results after determining a winner
         '''
         self.print_message(self.results_message())
         self.deck = []
 
     def make_card_deck(self, colors=DEFAULT_COLORS, num_per_color=3):
+        '''
+        make_card_deck makes a deck of cards, can pass in an array of colors and make the deck
+        larger by creating more cards per color. The default creates 9 cards.
+
+        Card.COLOR_VALUES shows the colors with set values Any other colors will get the color value
+        of 8.... which is pretty high
+
+        :param colors: colors of cards, defaults to DEFAULT_COLORS
+        :type colors: list of strings (color names). optional
+        :param num_per_color: based on this will increase the number of cards per color, defaults to 3
+        :type num_per_color: int, optional
+        '''
         self.colors_used = colors
         new_deck = []
         nums = range(1, num_per_color + 1)
@@ -96,10 +107,31 @@ class CardGame:
         self.deck = new_deck
 
     def sort_deck(self, colors=DEFAULT_COLORS):
+        '''
+        sort_deck sorts the cards by the color index, in the list
+        For example, if you use ['yellow', 'green', 'red'] it will be as follows:
+        * yellow will sort before green and red
+        * green will sort before red
+        * red will sort last
+
+        Then it will sort by card number (each card with a given color has an identical range of numbers)
+
+        :param colors: list of color names, defaults to DEFAULT_COLORS
+        :type colors: list of strings, optional
+        '''
         self.assign_color_sort(colors)
         self.deck.sort()
 
     def assign_color_sort(self, colors):
+        '''
+        assign_color_sort This assigns the index of the array to the cards color_sort property
+
+        if the list of colors is missing a color, the color_sort property will be 2000
+        There would have to be an awfully long list of colors, therefore no test for the list length
+
+        :param colors: list of color names (assumption that list will be shorter than 2000)
+        :type colors: list of strings
+        '''
         message = self.check_for_discrepency(colors)
         if message:
             self.print_message(message, False)
@@ -111,6 +143,19 @@ class CardGame:
                 card.assign_new_color_sort(2000)
 
     def check_for_discrepency(self, colors):
+        '''
+        check_for_discrepency lets user know if they are sorting by a color that is not in the deck
+        It also lets the user know if one of the colors in the deck is missing from the sort
+
+        It also tells the user the result of the mistake
+
+        These errors are dealt with.  The program continues.
+
+        :param colors: list of colors to sort by
+        :type colors: list of strings
+        :return: list of strings (message)
+        :rtype: message
+        '''
         msg = []
         all_colors_in_cards = all(item in self.colors_used for item in colors)
         all_card_colors_in_colors = all(item in colors for item in self.colors_used)
@@ -123,6 +168,15 @@ class CardGame:
         return msg
 
     def time_to_stop(self):
+        '''
+        time_to_stop returns True if the turns can not be evenly distributed among the players
+        Otherwise return False
+
+        It also changes the user who's turn it is
+
+        :return: True if its time to stop otherwise False
+        :rtype: bool
+        '''
         if not self.equal_turns():
             return True
         if self.num_turns > 0:
@@ -130,16 +184,42 @@ class CardGame:
         return False
 
     def equal_turns(self):
+        '''
+        equal_turns determines if there are enough turns for every player
+
+        :return: number of turns left where everyone gets a turn
+        :rtype: int
+        '''
         return len(self.deck)//len(self.players)
 
     def results_message(self):
+        '''
+        results_message is the message saying who wins
+
+        :return: list of strings (each gets its own print statement)
+        :rtype: list of strings
+        '''
         return [f'After {self.num_turns} turns, {self.find_winner()} won!!!!']
 
     def turn_message(self):
+        '''
+        run_message is the message saying whose turn it is and how to stop the game
+
+        :return: list of strings (each gets its own print statement)
+        :rtype: list of strings
+        '''
         return [f'Take turn number {self.num_turns + 1}, {self.players[self.turn_idx]}.  ',
                 'Press "q" or "Q" then <enter> (no quotes) to quit, otherwise just press <enter> ']
 
     def print_message(self, message, print_scores=True):
+        '''
+        print_message prints an array of strings.  Each has it's own print statement
+
+        :param message: list of messages
+        :type message: list of strings
+        :param print_scores: whether or not to print the score sheet, defaults to True
+        :type print_scores: bool, optional
+        '''
         if print_scores:
             print('Current Score Sheet: ')
             printer = pprint.PrettyPrinter(indent=1, width=120)
@@ -149,6 +229,12 @@ class CardGame:
             print(msg)
 
     def find_winner(self):
+        '''
+        find_winner finds the person with the highest score.  It also looks for ties
+
+        :return: player name[s] with the highest score
+        :rtype: string
+        '''
         max_value = max(self.score_sheet.values())
         max_list = [k for k, v in self.score_sheet.items() if v == max_value]
         if not max_list:
@@ -156,6 +242,17 @@ class CardGame:
         return ' and '.join(max_list)
 
     def take_turn(self, player):
+        '''
+        take_turn removes the card from the top of the card list
+
+        Adds the card score to the score sheet for the given player
+        Increment num_turns, so we know how many turns have been played
+
+        :param player: player name (use to add score)
+        :type player: string
+        :return: card that is played
+        :rtype: Card
+        '''
         try:
             card_to_play = self.deck.pop()
         except IndexError:
@@ -165,6 +262,9 @@ class CardGame:
         return card_to_play
 
     def next_turn(self):
+        '''
+        next_turn changes the player who just had a turn to the next one
+        '''
         max = len(self.players) - 1
         if self.turn_idx == max:
             self.turn_idx = 0
@@ -172,4 +272,7 @@ class CardGame:
             self.turn_idx += 1
 
     def shuffle_deck(self):
+        '''
+        shuffle_deck scrambles the deck
+        '''
         random.shuffle(self.deck)
